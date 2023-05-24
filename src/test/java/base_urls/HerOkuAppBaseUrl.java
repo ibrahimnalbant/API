9c6192104f10d0049da8f8ba23af6c832a9d54ce
ibrahim nalbant <nalbantibrahim@yahoo.com>
2023-05-24 23:38:08 +0300

    ibr230524-01

---
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -0,0 +1,57 @@
+package base_urls;
+
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class HerOkuAppBaseUrl {
+
+    protected RequestSpecification spec;
+
+    @Before
+    public void setUp() {
+        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com/").build();
+    }
+
+}
+
+/*
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class HerOkuAppBaseUrl {
+    //setUp() methodu ile request işleminde tekrarlı şekilde yapılacak işlemler burada bir kez yapılacak.
+        // @Test öncesi çalışması için @Before anatasyonu ekliyoruz.
+
+    protected RequestSpecification spec;
+
+    @Before//Her @Test methodu öncesei çalışır
+    public void setUp() {
+        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").setAccept(ContentType.JSON).build();
+    }
+
+}
+*/
+
+/*
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class JsonPlaceHolderBaseUrl {
+    //setUp() methodu ile request işleminde tekrarlı şekilde yapılacak işlemler burada bir kez yapılacak.
+        // @Test öncesi çalışması için @Before anatasyonu ekliyoruz.
+
+    protected RequestSpecification spec;
+
+    @Before//Her @Test methodu öncesei çalışır
+    public void setUp() {
+        spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com/").build();
+    }
+}
+
+ */
