08d27d69ae2c0110fbbbb3ca8be3661319b83906
ibrahim nalbant <nalbantibrahim@yahoo.com>
2023-05-25 23:20:30 +0300

    ibr230525-01

--- src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -6,14 +6,17 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
 public class JsonPlaceHolderBaseUrl {
-    //setUp() methodu ile request işleminde tekrarlı şekilde yapılacak işlemler burada bir kez yapılacak.
-        // @Test öncesi çalışması için @Before anatasyonu ekliyoruz.
+    //setUp() methodu ile request işleminde tekrarlı şekilde yapılacak işlemler burada bir kez yapılacak. @Test öncesi çalışması için @Before anatasyonu ekliyoruz.
 
     protected RequestSpecification spec;
 
     @Before//Her @Test methodu öncesei çalışır
     public void setUp() {
-        spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com/").setAccept(ContentType.JSON).build();
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri("https://jsonplaceholder.typicode.com/")
+                .setAccept(ContentType.JSON)
+                .build();
     }
 
 }
