9061edafbe8ec01d13d29443572a448d2eb3c9ba
ibrahim nalbant <nalbantibrahim@yahoo.com>
2023-05-28 23:26:51 +0300

    ibr20230528-01

--- src/test/java/base_urls/GoRestBaseUrl.java
+++ src/test/java/base_urls/GoRestBaseUrl.java
@@ -6,13 +6,13 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
-public class HerOkuAppBaseUrl {
+public class GoRestBaseUrl {
 
     protected RequestSpecification spec;
 
     @Before
     public void setUp() {
-        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com").build();
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://gorest.co.in/public/v1/").build();
     }
 
 }
