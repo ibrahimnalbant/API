1ef394471cf8fe70681d99b80ae89751a82c7e65
ibrahim nalbant <nalbantibrahim@yahoo.com>
2023-06-01 23:21:38 +0300

    ibr20230601-01

--- src/test/java/base_urls/HerOkuAppBaseUrl.java
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -6,16 +6,41 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
+import static utils.AuthenticationHerOkuApp.generateToken;
+
 public class HerOkuAppBaseUrl {
 
     protected RequestSpecification spec;
 
     @Before
     public void setUp() {
-        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com").build();
+        spec = new RequestSpecBuilder()
+                .addHeader("Cookie","token="+generateToken())
+                .setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com")
+                .build();
     }
 
 }
+
+
+/*
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class HerOkuAppBaseUrl {
+
+    protected RequestSpecification spec;
+
+    @Before
+    public void setUp() {
+        spec = new RequestSpecBuilder().addHeader("Cookie","token=8c24a829b1fa015").setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com").build();
+    }
+
+}
+*/
 /*
 
 import io.restassured.builder.RequestSpecBuilder;
--- src/test/java/test_data/HerOkuAppTestData.java
+++ src/test/java/test_data/HerOkuAppTestData.java
@@ -14,6 +14,54 @@ public class HerOkuAppTestData {
         return bookingdatesMap;
     }
 
+    public Map<String, Object> expectedDataMapMethod(String firstname, String lastname, Integer totalprice, Boolean depositpaid, Map<String, String> bookingdatesMap, String additionalneeds) {
+
+        Map<String, Object> expectedData = new HashMap<>();
+
+        if (firstname!=null){
+            expectedData.put("firstname", firstname);
+        }
+
+        if (lastname!=null){
+            expectedData.put("lastname", lastname);
+        }
+
+        if (totalprice!=null){
+            expectedData.put("totalprice", totalprice);
+        }
+
+        if(depositpaid!=null){
+            expectedData.put("depositpaid", depositpaid);
+        }
+
+        if(bookingdatesMap!=null){
+            expectedData.put("bookingdates", bookingdatesMap);
+        }
+
+        if (additionalneeds != null) {
+            expectedData.put("additionalneeds", additionalneeds);
+        }
+
+        return expectedData;
+    }
+
+}
+
+/*
+import java.util.HashMap;
+import java.util.Map;
+
+public class HerOkuAppTestData {
+
+    public Map<String, String> bookingdatesMapMethod(String checkin, String checkout) {
+
+        Map<String, String> bookingdatesMap = new HashMap<>();
+        bookingdatesMap.put("checkin", checkin);
+        bookingdatesMap.put("checkout", checkout);
+
+        return bookingdatesMap;
+    }
+
     public Map<String, Object> expectedDataMapMethod(String firstname, String lastname, int totalprice, boolean depositpaid, Map<String, String> bookingdatesMap, String additionalneeds) {
 
         Map<String, Object> expectedData = new HashMap<>();
@@ -32,6 +80,7 @@ public class HerOkuAppTestData {
     }
 
 }
+*/
 
 /*
 import java.util.HashMap;
