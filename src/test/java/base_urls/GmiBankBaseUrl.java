e17596eb26e1f7b3dfb1e1f199edb9bf1ac7f433
ibrahim nalbant <nalbantibrahim@yahoo.com>
2023-06-04 23:23:16 +0300

    ibr20230604-01

--- src/test/java/base_urls/GmiBankBaseUrl.java
+++ src/test/java/base_urls/GmiBankBaseUrl.java
@@ -5,17 +5,35 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
+import static utils.AuthenticationGmiBank.generateToken;
+
 public class GmiBankBaseUrl {
 
     protected RequestSpecification spec;
 
     @Before
     public void setUp() {
-        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).addHeader("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJrX3R3YWluIiwiYXV0aCI6IlJPTEVfQURNSU4sUk9MRV9NQU5BR0VSIiwiZXhwIjoxNjg1OTA3NTA0fQ.HA-zTF9SmAbzJcZfjpK2zgOWPegxXlr3qqTBURKD-4JN_uJDXBQRKXBlsmxqtVeRhDwGoTMgrStie-FKdE9KJQ").setBaseUri("https://gmibank.com").build();
+        spec = new RequestSpecBuilder().
+                setContentType(ContentType.JSON).
+                addHeader("Authorization", "Bearer " +generateToken() ).
+                setBaseUri("https://gmibank.com").build();
+    }
+
+}
+
+/*
+    @Before
+    public void setUp() {
+        spec = new RequestSpecBuilder().
+        setContentType(ContentType.JSON).
+        addHeader("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJrX3R3YWluIiwiYXV0aCI6IlJPTEVfQURNSU4sUk9MRV9NQU5BR0VSIiwiZXhwIjoxNjg1OTA3NTA0fQ.HA-zTF9SmAbzJcZfjpK2zgOWPegxXlr3qqTBURKD-4JN_uJDXBQRKXBlsmxqtVeRhDwGoTMgrStie-FKdE9KJQ").
+        setBaseUri("https://gmibank.com").build();
     }
 
 }
 //contentType(ContentType.JSON).header("Authorization","Bearer
+
+*/
 
 /*
 import io.restassured.builder.RequestSpecBuilder;
